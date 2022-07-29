8c751d5b296d8224518b81a19c5042b48e249a56
Ялымов В <yalymov@tprs.ru>
2022-07-29 09:46:43 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Consumer and Producer for logs
+Consumer and Producer for logs in rest projects
 =============================================
 This project contains a consumer what read queues in rabbit and put them to the mongo db.
 
@@ -9,11 +9,54 @@ In the package logger you can find the pusher what put message to the rabbit
 
 <pre>
     // download log_consumer
-    git clone 
+    git clone https://github.com/VladimirYalumov/log_consumer.git
+
     // download and run environment
-    git clone 
+    git clone https://github.com/VladimirYalumov/docker_pgdb_mongodb_redis.git
+    cd docker_pgdb_mongodb_redis
+    docker-compose up -d
 
     // go to project
-    cd 
-    
+    cd log_consumer
+    go mod vendor
+    go run main.go
+</pre>
+
+<h3>How to test?</h3>
+<h5>spoiler: even easier than start</h5>
+
+<pre>
+    // go to project test client
+    cd log_consumer/test_client
+    go run main.go
+    curl --location --request GET '127.0.0.1:8089/numbers_sum' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{
+        "first_num": 1,
+        "second_num": 2
+    }'
+</pre>
+And watch the result in the same mongo client?
+
+Configs
+in the block config (if you use my environment) you can't change anything
+In the block mongo you must add your own logical mongo databases
+And add all routes you have in project
+Example:
+<pre>
+mongo:
+    auth:
+        signin: signin
+        signup: signup
+        signout: signout
+    event:
+        add: add_event
+        update: update_event
+        delete: delete_event
+</pre>
+
+P. S. Consumer create the collection with current time of incoming log.
+Example:
+<pre>
+    update_event_202207
 </pre>
--- main.go
+++ main.go
@@ -9,27 +9,26 @@ import (
 	"log_consumer/mongo"
 	"log_consumer/rabbit"
 	"strconv"
-	"sync"
 )
 
 const queueCount = 5
 
 type Configurations struct {
-	Config Config `yaml:"config"`
-	Mongo map[string]interface{} `yaml:"mongo"`
+	Config Config                 `yaml:"config"`
+	Mongo  map[string]interface{} `yaml:"mongo"`
 }
 
 type Config struct {
 	RabbitHostPort string `yaml:"rabbit_host_port"`
-	RabbitUser string `yaml:"rabbit_user"`
+	RabbitUser     string `yaml:"rabbit_user"`
 	RabbitPassword string `yaml:"rabbit_pass"`
-	MongoHostPort string `yaml:"mongo_host_port"`
-	MongoUser string `yaml:"mongo_user"`
-	MongoPassword string `yaml:"mongo_pass"`
+	MongoHostPort  string `yaml:"mongo_host_port"`
+	MongoUser      string `yaml:"mongo_user"`
+	MongoPassword  string `yaml:"mongo_pass"`
 }
 
 type ConsumersInfo struct {
-	QueueName string
+	QueueName      string
 	ConsumersCount int
 }
 
@@ -47,10 +46,14 @@ var LogConsumer []Consumer
 
 func init() {
 	yfile, parseErr := ioutil.ReadFile("configurations.yml")
-	if parseErr != nil {panicIfNeed(parseErr, "")}
+	if parseErr != nil {
+		panicIfNeed(parseErr, "")
+	}
 	configurations := Configurations{}
 	parseErr = yaml.Unmarshal(yfile, &configurations)
-	if parseErr != nil {panicIfNeed(parseErr, "")}
+	if parseErr != nil {
+		panicIfNeed(parseErr, "")
+	}
 
 	mainError = RClient.SetConnection(
 		configurations.Config.RabbitUser,
@@ -80,34 +83,30 @@ func init() {
 
 // consumer
 func main() {
-	var wg sync.WaitGroup
 	// declare all queues
 	for i, consumerInfo := range consumersArray {
 		LogConsumer[i] = Consumer{}
 		LogConsumer[i].Client = &RClient
 		mainError = LogConsumer[i].CreateQueue(consumerInfo.QueueName)
-		panicIfNeed(mainError, "declare queue: " + consumerInfo.QueueName)
+		panicIfNeed(mainError, "declare queue: "+consumerInfo.QueueName)
 		for j := 0; j < consumerInfo.ConsumersCount; j++ {
-			wg.Add(1)
-			go LogConsumer[i].Execute(&wg, consumerInfo.QueueName + "_" + strconv.Itoa(j))
+			go LogConsumer[i].Execute(consumerInfo.QueueName + "_" + strconv.Itoa(j))
 		}
 	}
-	wg.Wait()
 
 	mainError = RClient.CloseConnection()
 	panicIfNeed(mainError, "log_consumer stopped")
 }
 
-func (consumer *Consumer) Execute(wg *sync.WaitGroup, name string) {
-	defer wg.Done()
+func (consumer *Consumer) Execute(name string) {
 	var distributeErr error
 	msgs, err := consumer.Client.Chanel.Consume(
 		consumer.Queue.Name, // queue
-		name,                  // consumer
+		name,                // consumer
 		true,                // auto-ack
 		false,               // exclusive
 		false,               // no-local
-		true,               // no-wait
+		true,                // no-wait
 		nil,                 // args
 	)
 
@@ -130,12 +129,12 @@ func (consumer *Consumer) Execute(wg *sync.WaitGroup, name string) {
 func (consumer *Consumer) CreateQueue(name string) error {
 	var err error
 	consumer.Queue, err = consumer.Client.Chanel.QueueDeclare(
-		name, // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		name,  // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	return err
 }
